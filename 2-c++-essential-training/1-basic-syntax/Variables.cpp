c0045b509bb8a9e45dbcc4ad997c0cb377d2e35c
Mehmet Tarhan <hi@memtarhan.com>
2021-05-01 18:04:00 +0300

    [C++ Essentials] - Brief description of variables

---
+++ 2-c++-essential-training/1-basic-syntax/Variables.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main()
+{
+    int number; // If not assigned it's 0 by default
+    float decimal; // If not assigned it's 0.0000... by default
+    bool boolean; // If not assigned it's false by default
+
+    printf("Number: %d\n", number);
+    printf("Decimal: %f\n", decimal);
+    printf("Boolean: %d\n", boolean);
+
+    return 0;
+}
